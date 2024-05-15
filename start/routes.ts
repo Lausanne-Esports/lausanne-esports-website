f6c041be9cf868f8749ea5456052b7284f3f33e0
Romain Lanz <romain.lanz+github@pm.me>
2024-05-15 20:32:58 +0200

    chore: initial commit

---
+++ start/routes.ts
@@ -0,0 +1,11 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+router.on('/inertia').renderInertia('home', { version: 6 })
